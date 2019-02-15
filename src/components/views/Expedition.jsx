dccd4cf5226711421ef8554f8286149821317d59
robinsongz <mrrobinsung@gmail.com>
2019-02-15 12:54:40 -0800

    reversed views changes, kept new exped pic and process.env

--- src/components/views/Expedition.jsx
+++ src/components/views/Expedition.jsx
@@ -6,7 +6,6 @@ class Expedition extends Component {
   render() {
 
     return (
-      
       <div className="expedition view-comp">
 
         <h2>
@@ -15,23 +14,23 @@ class Expedition extends Component {
 
 
         <h3>
-          Set your checkInChannel config
+          set checkInChannel Config
         </h3>
-        <div className="description">
-          !team command will only be available in this channel
-         </div> 
-        <div className="picture">
-          <img alt='checkInChannel example' src={require('../Pictures/check in channel example.png')} />
+        <div className="section">
+          <div>
+            !team command will only be available in this channel
+          </div>
+          <img alt="checkInChannel" src={require('../Pictures/checkin example.png')} />
         </div>
 
-
+        
         <h3>
           !team checkin
         </h3>
-        <div className="description">        
-          check-in to first available team
-        </div>  
-        <div className="picture">
+        <div className="section">        
+          <div>
+            check-in to first available team
+          </div>
           <img alt='gmrole' src={require('../Pictures/checkin example.png')} />
         </div>
 
@@ -39,50 +38,50 @@ class Expedition extends Component {
         <h3>
           !team checkout
         </h3>
-        <div className="description">
+        <div className="section">
+          <div>
             check-out from team
-        </div>
-        <div className="picture">
+          </div>
           <img alt='checkout' src={require('../Pictures/checkout example.png')} />
         </div>
 
         <h3>
           !team view all
         </h3>
-        <div className="description">
-          view all teams
-        </div>
-        <div className="picture">
+        <div className="section">
+          <div>
+            view all teams
+          </div>
           <img alt='team view all' src={require('../Pictures/team view all example.png')} />
         </div>
 
         <h3>
           !team edit team[#] [team title]
         </h3>
-        <div className="description">
+        <div className="section">
+          <div>
             changing team #'s title
           </div>
-        <div className="picture">
           <img alt='team name change' src={require('../Pictures/changing team names example.png')} />
         </div>
 
         <h3>
           !team add team[#] [ign]
         </h3>
-        <div className="description">
-          adding [ign] to team #
-        </div>
-        <div className="picture">
+        <div className="section">
+          <div>
+            adding [ign] to team #
+          </div>
           <img alt='team add' src={require('../Pictures/team add exmaple.png')} />
         </div>
 
         <h3>
           !team mention team[#] [message]
         </h3>
-        <div className="description">
+        <div className="section">
+          <div>
             sends [message] to everyone on team #
-        </div>
-        <div className="picture">
+          </div>
           <img alt='team mention' src={require('../Pictures/team mention example.png')} />
         </div>
 
--- src/components/views/GettingStarted.jsx
+++ src/components/views/GettingStarted.jsx
@@ -12,17 +12,18 @@ class GettingStarted extends Component {
           Setting Configurations
         </h2>
 
-        <div className="description">        
-          Give yourself the role of 'GM' to start setting configs.
-        </div>
-        <div className="picture">
+        <div className="section">        
+          <div>
+            Give yourself the role of 'GM' to start setting configs.
+          </div>
+
           <img alt='gmrole' src={require('../Pictures/gmrole.png')} />
         </div>
 
         <h3>
           Type !showconf to view your current configs. These are the default configs -
         </h3>
-        <div className="picture">        
+        <div className="section">        
 
           <img alt='configuration' src={require('../Pictures/showconf.png')} />
         </div>
@@ -32,69 +33,52 @@ class GettingStarted extends Component {
         <h3>
           Configuration Help
         </h3>
-        <div className="picture">
-          <img alt='config tutorial' src={require('../Pictures/config tutorial.png')} />
+        <div className="section">
+          <img alt='keys' src={require('../Pictures/config tutorial.png')} />
         </div>
 
 
         <h3>
           What each configuration means
         </h3>
-        <div className="picture">        
+        <div className="section">        
           <img alt='keys'src={require('../Pictures/keys.png')}/>
         </div>
 
         <h3>
           EXAMPLES
         </h3>
-        <div className="description bold-alert">
-          If you want expedition reminders to send to a channel called 'expeditions'
-        </div>
+        <div className="section">
 
-        <div className="picture">
+          <div className="bold-alert">If you want expedition reminders to send to a channel called 'expeditions'</div>
           <img alt='expoChannel example' src={require('../Pictures/expochannel example.png')} />
-        </div>
 
+          <div>
+            <div className="bold-alert">Changing guild fort reminder message</div>
+          </div>
+          <img alt='keys' src={require('../Pictures/guild fort example.png')}/>
+
+          <div >
+            <div className="bold-alert">Changing banquet reminder time. </div>
+            <div className="bold-alert">banquetTime is formatted by [minute][hour] in server time </div>
+            <div className="bold-alert">00 19 = 7:00pm server time</div>
+          </div>
+          <img alt='keys' src={require('../Pictures/banquetTime example.png')} />
+
+          <div >
+            <div className="bold-alert">Changing adminRole from 'GM' to 'Slime Boss'.</div>
+          </div>
+          <img alt='keys' src={require('../Pictures/adminRole example.png')} />
+
+          <div >
+            <div className="bold-alert">Now give yourself the role of 'Slime Boss' to use admin commands.</div>
+          </div>
+          <img alt='keys' src={require('../Pictures/slimebossexample.png')} />
         
-          
-        <div className="description bold-alert">
-          Changing guild fort reminder message
-        </div>
-        <div className="picture">
-          <img alt='guild fort example' src={require('../Pictures/guild fort example.png')}/>
         </div>
 
-        <div className="description">
-          <div className="bold-alert">Changing banquet reminder time. </div>
-          <div className="bold-alert">banquetTime is formatted by [minute][hour] in server time </div>
-          <div className="bold-alert">00 19 = 7:00pm server time</div>
-        </div>
 
-        <div className="picture">
-          <img alt='banquetTime example' src={require('../Pictures/banquetTime example.png')} />
-        </div>
-
-          
-        <div className=" description bold-alert">
-          Changing adminRole from 'GM' to 'Slime Boss'.
-        </div>
-        
-        <div className="picture">
-          <img alt='adminRole example' src={require('../Pictures/adminRole example.png')} />
-        </div>
-
-          
-        <div className=" description bold-alert">
-          Now give yourself the role of 'Slime Boss' to use admin commands.
-        </div>
-        
-        <div className="picture">
-          <img alt='slime boss example' src={require('../Pictures/slimebossexample.png')} />
-        </div>
       </div>
-
-
-      
 
 
     )
