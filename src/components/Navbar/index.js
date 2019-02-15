356322c25c22011011edefa7d935c50265ab5776
robinsongz <mrrobinsung@gmail.com>
2019-02-14 16:13:52 -0800

    changed font, added margin, capitalize links

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,19 +6,19 @@ const Navbar = () => (
   <div className="navbar-comp">
     <div className="main-link">
       <NavLink to={process.env.PUBLIC_URL + '/'}>
-        <h4>Slime Bot</h4>
+        <h4>SLIME BOT</h4>
       </NavLink>
     </div>
     <div className="navbar-links">
       <NavLink
         to='/gettingstarted'
         activeClassName="active-link">
-        <div className="nav-link">Getting Started</div>
+        <div className="nav-link">GETTING STARTED</div>
       </NavLink>
       <NavLink
         to='/commands'
         activeClassName="active-link">
-        <div className="nav-link">Commands</div>
+        <div className="nav-link">COMMANDS</div>
       </NavLink>
     </div>
   </div>
