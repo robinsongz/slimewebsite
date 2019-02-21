c483bb3e5dd6ba4328f33f993c03a136384be04c
Yufan Wang <yufanwangcn@hotmail.com>
2019-02-20 16:47:41 -0800

    fixes to forge page, view page image styles

--- src/components/views/Forge.jsx
+++ src/components/views/Forge.jsx
@@ -14,49 +14,51 @@ class Forge extends Component {
         <h3>
           !fuse [Fuse Item] [Fuse Material] [Fuse Material Cost]
         </h3>
-
-        <div className="description">
-          
+        <div className="section">
+          <div className="description">
+            
             Fusing Cost Calculator
           </div>
-        <div className="picture">
-          <img alt='fusing cost calculator' src={require('../Pictures/fusing cost calc example.png')} />
+          <div className="picture">
+            <img alt='fusing cost calculator' src={require('../Pictures/fusing cost calc example.png')} />
+          </div>
         </div>
         
         <h3>
           !fuse treasure
         </h3>
-
-        <div className="description">
-          
+        <div className="section">
+          <div className="description">
             Treasure Pulls Fusing Cost
           </div>
           <div className="picture">
-          <img alt='treasure pulls' src={require('../Pictures/treasure example.png')} />
+            <img alt='treasure pulls' src={require('../Pictures/treasure example.png')} />
+          </div>
         </div>
         
         <h3>
           !fuse data
         </h3>
-
-        <div className="description">
-          
+        <div className="section">
+          <div className="description">      
             General Fusing Data
           </div>
           <div className="picture">
-          <img alt='fusing data' src={require('../Pictures/fusing data example.png')} />
+            <img alt='fusing data' src={require('../Pictures/fusing data example.png')} />
+          </div>
         </div>
         
         <h3>
           !jewel [color]
         </h3>
-
-        <div className="description">
+        <div className="section">   
+          <div className="description">
           
             Jewel Data
           </div>
           <div className="picture">
-          <img alt='jewel example' src={require('../Pictures/jewel example.png')} />
+            <img alt='jewel example' src={require('../Pictures/jewel example.png')} />
+          </div>
         </div>
 
         
@@ -64,13 +66,16 @@ class Forge extends Component {
           !jewel [color] setbonus
         </h3>
 
-        <div className="description">
-          
+        <div className="section">
+          <div className="description">
+            
             Jewel Set Bonus Data
           </div>
           <div className="picture">
-          <img alt='team add' src={require('../Pictures/jewel setbonus exmaple.png')}/>
+            <img alt='team add' src={require('../Pictures/jewel setbonus exmaple.png')}/>
+          </div>
         </div>
+
 
       </div>
 
