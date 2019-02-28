6af252a3f65db9e89164a7e72566aea9dfedaaa3
robinsongz <mrrobinsung@gmail.com>
2019-02-28 14:36:06 -0800

    changed front page and button

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,38 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="slime-img">
-          <img
-            alt='slime logo'
-            src={require('./Pictures/king_slime_solo.png')} />
+          <div className="slime-img">
+            <img
+              alt='slime logo'
+              src={require('./Pictures/king_slime_solo.png')} />
+          </div>
+          <div>
+            <h1 className="slime-heading">
+              A DISCORD BOT FOR MAPLESTORY M
+          </h1>
+          <h2>
+            Organize Expeditions
+          </h2>
+          <h2>
+            Exped, Guild Fort, & Banquet Reminders
+          </h2>
+          <h2>
+            Multiple Data on Command
+          </h2>
+          </div>
+          <div>
+            <a
+              href='https://discordapp.com/oauth2/authorize?client_id=502644283329871872&scope=bot'
+              target='blank'
+              className="discord-link"
+            >
+            <h2>
+              ADD TO YOUR SERVER
+            </h2>
+            
+            </a>
+          </div>
         </div>
-        <div>
-          <h1 className="slime-heading">
-            A DISCORD BOT FOR MAPLESTORY M
-        </h1>
-        </div>
-        <div>
-          <a
-            href='https://discordapp.com/oauth2/authorize?client_id=502644283329871872&scope=bot'
-            target='blank'
-            className="discord-link"
-          >
-            Invite Now
-          </a>
-
-
-        </div>
-      </div>
-
     )
   }
 }
