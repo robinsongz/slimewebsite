debaab12495eb4287c858494d03b14dfbd3cb016
robinsongz <mrrobinsung@gmail.com>
2019-02-27 18:23:18 -0800

    import changed to js

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import App from './components/App';
-import Documentation from './components/Documentation/index.jsx';
+import Documentation from './components/Documentation/index.js';
 
 const Routes = () => {
   return (
