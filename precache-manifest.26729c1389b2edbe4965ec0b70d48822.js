f6072ebc4028b91f0a4a27ebfd3de030cdf54147
robinsongz <mrrobinsung@gmail.com>
2019-02-27 18:26:41 -0800

    Updates

---
+++ precache-manifest.26729c1389b2edbe4965ec0b70d48822.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "d13936322876a227825f9d6d8a340f74",
+    "url": "/static/media/slimebossexample.d1393632.png"
+  },
+  {
+    "revision": "6223e32aa980ac1b99fa",
+    "url": "/static/css/main.bc2225ff.chunk.css"
+  },
+  {
+    "revision": "5540df14ed53f1e6afd1",
+    "url": "/static/js/1.5540df14.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0fac2bfdd8b01665c956786468701b30",
+    "url": "/static/media/checkin example.0fac2bfd.png"
+  },
+  {
+    "revision": "4a235e8439e0c0dea155a7e016d1cd6f",
+    "url": "/static/media/king_slime_solo.4a235e84.png"
+  },
+  {
+    "revision": "08236a2e93fc63cc1e3d66456a5ad19c",
+    "url": "/static/media/gmrole.08236a2e.png"
+  },
+  {
+    "revision": "9273a68b203d3d37fc869823c99f4ec0",
+    "url": "/static/media/showconf.9273a68b.png"
+  },
+  {
+    "revision": "5c6d09197c8dfa3bf92e8a77f338d0c3",
+    "url": "/static/media/config tutorial.5c6d0919.png"
+  },
+  {
+    "revision": "f5665d1bb8aa48ae2b871fdb333f0121",
+    "url": "/static/media/keys.f5665d1b.png"
+  },
+  {
+    "revision": "8dc506d669d53928c6f3cf518794afeb",
+    "url": "/static/media/expochannel example.8dc506d6.png"
+  },
+  {
+    "revision": "ace3aecc83dd27a5485c1b83b79355cd",
+    "url": "/static/media/guild fort example.ace3aecc.png"
+  },
+  {
+    "revision": "893190d333394a8ed1553efcb31098eb",
+    "url": "/static/media/banquetTime example.893190d3.png"
+  },
+  {
+    "revision": "ab399aa041984d515acf070dc6cdaf89",
+    "url": "/static/media/adminRole example.ab399aa0.png"
+  },
+  {
+    "revision": "6223e32aa980ac1b99fa",
+    "url": "/static/js/main.6223e32a.chunk.js"
+  },
+  {
+    "revision": "df73a5e153a714274640f3898dc61437",
+    "url": "/static/media/checkout example.df73a5e1.png"
+  },
+  {
+    "revision": "b1e50af8bc08c3973c4289f85db54504",
+    "url": "/static/media/team view all example.b1e50af8.png"
+  },
+  {
+    "revision": "c81b5cad4dfc079ef808652ef56da771",
+    "url": "/static/media/changing team names example.c81b5cad.png"
+  },
+  {
+    "revision": "b54fc34b6c528677cf272d1e5d978365",
+    "url": "/static/media/team add exmaple.b54fc34b.png"
+  },
+  {
+    "revision": "0f32143b670118444648cb5e6e977a5a",
+    "url": "/static/media/team mention example.0f32143b.png"
+  },
+  {
+    "revision": "af456b4c3a487f363e5fe88828fa4f6a",
+    "url": "/static/media/fusing cost calc example.af456b4c.png"
+  },
+  {
+    "revision": "32037269f670205a3a8f230c5390e715",
+    "url": "/static/media/treasure example.32037269.png"
+  },
+  {
+    "revision": "42c8f103148091e2b2474afb5e144396",
+    "url": "/static/media/fusing data example.42c8f103.png"
+  },
+  {
+    "revision": "358cbda5c883ba2e4cce4fcbe2cccbf4",
+    "url": "/static/media/jewel example.358cbda5.png"
+  },
+  {
+    "revision": "5bb20952d8c6b5049e83ddc2b276000b",
+    "url": "/static/media/jewel setbonus exmaple.5bb20952.png"
+  },
+  {
+    "revision": "ca9f3afba901cc6772b215daf690b42e",
+    "url": "/static/media/preferences_settings_tools-512.ca9f3afb.png"
+  },
+  {
+    "revision": "71908c09a47a2f66e40fc4cf3c392e70",
+    "url": "/static/media/zakum.71908c09.png"
+  },
+  {
+    "revision": "4b7521bbf03e9eae973c8a2a0f0193c0",
+    "url": "/static/media/jbossforge_icon_256px.4b7521bb.png"
+  },
+  {
+    "revision": "7d18092d7b29a24cbd51f18b3c0dfdb5",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.22aabc87d17112ffaa41e3fb054c5c4a.js"
+  "/precache-manifest.26729c1389b2edbe4965ec0b70d48822.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
